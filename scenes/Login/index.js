c8188e1a17b98e35ccf28208247da1cf542d9cb0
tom-aglow <tom@ohhhh.me>
2018-04-13 22:53:13 +0300

    fetch user data from facebook api

--- scenes/Login/index.js
+++ scenes/Login/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Facebook } from 'expo';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+
+import { FACEBOOK_APP_ID } from '~/services/constants/auth';
 
 export default class Login extends React.Component {
+  handleFacebookButtonPress = async () => {
+    const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+      FACEBOOK_APP_ID,
+      {
+        permissions: ['public_profile', 'email', 'user_birthday'],
+      },
+    );
+
+    if (type === 'success') {
+      const response = await axios.get('https://graph.facebook.com/me', {
+        params: {
+          access_token: token,
+          fields: ['name', 'email', 'birthday', 'gender', 'picture'].join(','),
+        },
+      });
+      console.log(response);
+    }
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Login</Text>
+        <TouchableOpacity onPress={this.handleFacebookButtonPress}>
+          <Text>Login with Facebook</Text>
+        </TouchableOpacity>
       </View>
     );
   }
