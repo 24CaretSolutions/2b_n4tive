918b3924c8b0458a8645b8e6a02adf48d6894302
tom-aglow <tom@ohhhh.me>
2018-04-13 21:43:58 +0300

    add file path alias for project root

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -1,10 +1,9 @@
-/* eslint-disable react/jsx-filename-extension */
 import { TabNavigator } from 'react-navigation';
 
-import Search from '../../scenes/Search';
-import MyEvents from '../../scenes/MyEvents';
-import Profile from '../../scenes/Profile';
-import Login from '../../scenes/Login';
+import Search from '~/scenes/Search';
+import MyEvents from '~/scenes/MyEvents';
+import Profile from '~/scenes/Profile';
+import Login from '~/scenes/Login';
 
 export default TabNavigator(
   {
