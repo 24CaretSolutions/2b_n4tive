22b2a2d8e6402f53171cdc3e474d1880b8918fd2
tom-aglow <tom@ohhhh.me>
2018-04-13 21:47:32 +0300

    delete unnecessary styles of NavBar

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -5,20 +5,9 @@ import MyEvents from '~/scenes/MyEvents';
 import Profile from '~/scenes/Profile';
 import Login from '~/scenes/Login';
 
-export default TabNavigator(
-  {
-    Search: { screen: Search },
-    'My Events': { screen: MyEvents },
-    Profile: { screen: Profile },
-    Login: { screen: Login },
-  },
-  {
-    tabBarOptions: {
-      inactiveTintColor: 'gray',
-      labelStyle: {
-        flex: 1,
-        justifyContent: 'flex-start',
-      },
-    },
-  },
-);
+export default TabNavigator({
+  Search: { screen: Search },
+  'My Events': { screen: MyEvents },
+  Profile: { screen: Profile },
+  Login: { screen: Login },
+});
